1a1e1119de4c6c69ff4a6cea9278d2f55b04601d
ale_allexandra99 <ale_allexandra99@yahoo.com>
2020-04-22 18:19:49 +0300

    Add project files.

---
+++ EvidentaPersoane/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EvidentaPersoane
+{
+    class Program
+    { interface IPersoane
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
